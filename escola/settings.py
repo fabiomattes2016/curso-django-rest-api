5b840aa72596b77d1748b5e9db58e823424b874b
Fábio Mattes <fabiomattes2007@gmail.com>
2024-05-19 13:28:14 -0300

    :sparkles: feat: Habilitar token

--- escola/settings.py
+++ escola/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'cursos',
     'rest_framework',
+    'rest_framework.authtoken', # JWT
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # DRF
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLAESSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1,
 }
